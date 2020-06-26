f2ab4d9d4e88a46eb232b237d154414313396db2
Ryan <ryanlsmith@me.com>
2020-06-25 23:19:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -23,14 +23,15 @@ def streamer():
     pubsub.subscribe('mode', 'speed')
     while True:
         message = pubsub.get_message()
-        print(message)
-        print(message['channel'])
-        print(message['channel'].decode('utf-8'))
-        print(message['data'])
-        #print(message['data'].decode('utf-8'))
-        if message['channel'].decode('utf-8') == 'speed':
+        if message['type'] == 'message':
             print(message)
-            yield 'event: SPEED\ndata: {}\n\n'.format(message['data'].decode('utf-8'))
+            print(message['channel'])
+            print(message['channel'].decode('utf-8'))
+            print(message['data'])
+            #print(message['data'].decode('utf-8'))
+            if message['channel'].decode('utf-8') == 'speed':
+                print(message)
+                yield 'event: SPEED\ndata: {}\n\n'.format(message['data'].decode('utf-8'))
 
 if __name__ == 'app':
     poller.start()
