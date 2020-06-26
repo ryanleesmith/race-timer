9ea1f7a72bb25d14a3dd36cce3e6c4888fdd114e
Ryan Smith <ryan.smith@salesforce.com>
2020-06-25 19:05:45 -0700

    Update timer.py

--- timer.py
+++ timer.py
@@ -2,79 +2,39 @@ from gps import *
 import math
 import time
 import json
+import threading
+
+gpsd = None
+
+class Poller(threading.Thread):
+    def __init(self):
+        threading.Thread.__init__(self)
+
+        global gpsd
+        gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+        self.current_value = None
+        self.running = True
+ 
+    def run(self):
+        global gpsd
+        while poller.running:
+            gpsd.next()
 
 def timer():
-    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
-
-    start = 0
-    started = False
-    finished = False
-    prev_speed = 0
-    data = {}
-
-    speed = 0
-
+    poller = new Poller()
     try:
-        run = int(round(time.time() * 1000))
+        poller.start()
         while True:
-            report = gpsd.next()
-            print(report['class'])
-            #if report['class'] == 'DEVICES' or report['class'] == 'SKY':
-            #    print(report)
-                #continue
-            print(int(round(time.time() * 1000)) - run)
-            run = int(round(time.time() * 1000))
-            #if report['class'] == 'TPV':
-            #    print(report)
-            #speed = float(getattr(report, 'speed', 'nan'))
-            
+            speed = gpsd.fix.speed
             if math.isnan(speed):
                 speed = 0
-                yield 'event: STATUS\ndata: SPEED_UNKNOWN\n\n'
-
-            #speed = math.floor(speed * 2.237)
+            print(speed)
             dump = json.dumps({'x': int(round(time.time() * 1000)), 'y': speed})
-            #print(dump)
             yield 'event: SPEED\ndata: {}\n\n'.format(dump)
-
-            if speed == 0:
-                yield 'event: STATUS\ndata: READY\n\n'
-                start = time.time()
-                started = True
-                finished = False
-                prev_speed = 0
-                data = {}
-            elif started:
-                if speed > prev_speed:
-                    yield 'event: STATUS\ndata: TIMING\n\n'
-                    prev_speed = speed
-                    if speed >= 30 and '30' not in data:
-                        diff = time.time() - start
-                        data['30'] = diff
-                        dump = json.dumps({'30': diff})
-                        yield 'event: RESULT\ndata: {}\n\n'.format(dump)
-                    if speed >= 60 and '60' not in data:
-                        finished = True
-                        diff = time.time() - start
-                        data['60'] = diff
-                        dump = json.dumps({'60': diff})
-                        yield 'event: RESULT\ndata: {}\n\n'.format(dump)
-                else:
-                    prev_speed = 0
-                    started = False
-                    #if '30' not in data:
-                    #    data['30'] = 'N/A'
-                    #if '60' not in data:
-                    #    data['60'] = 'N/A'
-                    #yield "data: 30: {}\t60: {}\n\n".format(data['30'], data['60'])
-                if finished:
-                    yield 'event: STATUS\ndata: FINISHED\n\n'
-            elif not finished:
-                yield 'event: STATUS\ndata: NOT_READY\n\n'
-
-            speed = speed + 0.1
-            if speed >= 62:
-                speed = 60
-            #time.sleep(0.1)
+            
+            time.sleep(0.1)
     except (KeyboardInterrupt, SystemExit):
-        print("Done.\nExiting.")
+        print("\nKilling Thread...")
+        poller.running = False
+        poller.join()
