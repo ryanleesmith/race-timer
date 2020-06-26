1d2ee00463694513ff026170032c99ee02b3ca05
Ryan <ryanlsmith@me.com>
2020-06-25 23:31:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ def streamer():
     while True:
         message = pubsub.get_message()
         if message and message['type'] == 'message':
-            print(message)
-            print(message['channel'])
-            print(message['channel'].decode('utf-8'))
-            print(message['data'])
+            #print(message)
+            #print(message['channel'])
+            #print(message['channel'].decode('utf-8'))
+            #print(message['data'])
             #print(message['data'].decode('utf-8'))
             if message['channel'].decode('utf-8') == 'speed':
-                print(message)
+                #print(message)
                 yield 'event: SPEED\ndata: {}\n\n'.format(message['data'].decode('utf-8'))
 
 if __name__ == 'app':
