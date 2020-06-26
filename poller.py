1a766f1eba5e4d67aa42ab978bf648987ef593eb
Ryan <ryanlsmith@me.com>
2020-06-25 23:05:07 -0700

    Update poller.py

--- poller.py
+++ poller.py
@@ -7,6 +7,7 @@ import redis
 
 gpsd = None
 poller = None
+mode = None
 
 red = redis.StrictRedis()
 
@@ -25,32 +26,44 @@ class Poller(threading.Thread):
         while self.running:
             gpsd.next()
 
-def start():
-    global gpsd, poller
-    poller = Poller()
-    mode = None
+class Threader(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
 
-    print("start")
-
-    try:
-        poller.start()
-        while True:
-            print("loop")
-            speed = gpsd.fix.speed
-            if math.isnan(speed):
-                speed = 0
-            #print(speed)
-            #print(gpsd.fix.mode)
-            if gpsd.fix.mode != mode:
-                mode = gpsd.fix.mode
-                red.publish('mode', mode)
-            red.publish('speed', speed)
-            #print(gpsd.satellites)
-            #dump = json.dumps({'x': int(round(time.time() * 1000)), 'y': speed})
-            #yield 'event: SPEED\ndata: {}\n\n'.format(dump)
-            
-            time.sleep(0.1)
-    except (KeyboardInterrupt, SystemExit):
-        print("\nKilling Thread...")
+        self.current_value = None
+        self.running = True
+ 
+    def run(self):
+        global poller
+        poller = Poller()
+        try:
+            poller.start()
+            while True:
+                print("loop")
+                speed = gpsd.fix.speed
+                if math.isnan(speed):
+                    speed = 0
+                #print(speed)
+                #print(gpsd.fix.mode)
+                if gpsd.fix.mode != mode:
+                    mode = gpsd.fix.mode
+                    red.publish('mode', mode)
+                red.publish('speed', speed)
+                #print(gpsd.satellites)
+                #dump = json.dumps({'x': int(round(time.time() * 1000)), 'y': speed})
+                #yield 'event: SPEED\ndata: {}\n\n'.format(dump)
+                
+                time.sleep(0.1)
+        except (KeyboardInterrupt, SystemExit):
+        print("\nKilling Threader...")
         poller.running = False
         poller.join()
+
+def start():
+    threader = Threader()
+    try:
+        threader.start()
+    except (KeyboardInterrupt, SystemExit):
+        print("\nKilling main...")
+        threader.running = False
+        threader.join()
